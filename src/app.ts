22fa4847c0bd2419fb75755c44ad4d71f2161209
limtis0 <limtis2109@gmail.com>
2024-10-21 12:03:36 +0200

    Changed the error handler to be more specific

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors'
 import healthzRoutes from './features/healthz/routes';
+import changesRoutes from './features/changes/routes';
 
 async function bootstrap() {
     const app = fastify();
 
     app.setErrorHandler((error, request, reply) => {
         console.error(`Error while requesting ${request.url}: ${error}`);
-        return reply.code(500).send({ error: 'Internal server error' });
+
+        if (error.statusCode && error.statusCode >= 500) {
+            return reply.code(500).send({ error: 'Internal server error' });
+        }
+        else {
+            return reply.code(error.statusCode ?? 400).send({
+                error: error.message
+            });
+        }
+    
     });
 
     // ! Set up domain-name in production
@@ -15,6 +25,7 @@ async function bootstrap() {
 
     // TODO Add routes
     app.register(healthzRoutes);
+    app.register(changesRoutes);
 
     app.listen({ port: 3001 }, (error, address) => {
         if (error) {
